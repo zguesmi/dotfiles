a35c06ebf490377b1814731788d7accb57b2938d
Zied <26070035+zguesmi@users.noreply.github.com>
2020-05-10 02:07:26 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -3,6 +3,13 @@
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
+GR="\033[1;32m" # green
+NC="\033[0m"    # no color
+# PREFIX="<><><><>"
+
+function log() {
+    printf "\n${GR}<><><> ${1} <><><>${NC}\n"
+}
 
 function add_deb_repo() {
     local NAME=$1
@@ -33,16 +40,17 @@ function apt_update_install_quiet() {
 #################
 PACKAGES="apt-transport-https ca-certificates curl git gnupg-agent htop httpie jq nmap neofetch "
 PACKAGES+="powerline software-properties-common stow terminator tree unzip vim wget zip zsh"
-printf "> Installing those OS packages:\n$PACKAGES\n"
+log "Installing those OS packages:"
+printf "${PACKAGES}\n"
 apt_update_quiet && apt_install_quiet $PACKAGES
 
 #################
 #   Terminal    #
 #################
-printf "> Setting up terminal\n"
-printf "> Configuring zsh as the default shell for user: $USER\n"
+log "Setting up terminal"
+log "Configuring zsh as the default shell for user: $USER"
 chsh -s $(which zsh)
-printf "> Installing oh-my-zsh addons\n"
+log "Installing oh-my-zsh addons"
 git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git 	zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone -q https://github.com/zsh-users/zsh-autosuggestions			zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone -q https://github.com/Powerlevel9k/powerlevel9k.git 			zsh/.oh-my-zsh/custom/themes/powerlevel9k
@@ -51,13 +59,13 @@ git clone -q https://github.com/romkatv/powerlevel10k.git				zsh/.oh-my-zsh/cust
 #################
 #   Dev tools   #
 #################
-printf "> Installing dev tools\n"
+log "Installing dev tools"
 # Ansible
 sudo add-apt-repository --yes ppa:ansible/ansible && apt_update_install_quiet ansible
 # Docker
 sudo snap install docker
 COMPOSE_BIN="https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)"
-sudo curl -L $COMPOSE_BIN -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -fsSL $COMPOSE_BIN -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker user
 # Nodejs
 # sudo snap install node --classic
@@ -71,7 +79,7 @@ sdk install gradle 5.5
 #################
 #  Desktop apps #
 #################
-printf "> Installing desktop apps:\n"
+log "Installing desktop apps"
 sudo snap install codium --classic
 sudo snap install gitkraken
 sudo snap install postman
@@ -85,7 +93,7 @@ add_deb_repo "riot-desktop" \
         "deb https://packages.riot.im/debian/ default main" \
         "https://packages.riot.im/debian/riot-im-archive-keyring.gpg"
 # Shutter
-sudo add-apt-repository --yes ppa:linuxuprising/shutter
+sudo add-apt-repository --yes ppa:linuxuprising/shutter > /dev/null
 # Etcher
 sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
 add_deb_repo "balena-etcher" \
@@ -96,6 +104,6 @@ apt_update_install_quiet brave-browser riot-desktop shutter balena-etcher-electr
 #################
 #   Dotfiles    #
 #################
-printf "> Populating dotfiles\n"
+log "Populating dotfiles"
 stow fonts && sudo fc-cache -f
 stow zsh vscodium git
